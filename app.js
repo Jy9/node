0f280f20f529769b15210cbfefb8b7857daa7db9
jiayue <15601215226@163.com>
2019-12-13 19:57:46 +0800

    diyici tijiao

---
+++ app.js
@@ -0,0 +1,37 @@
+const Koa = require('koa2'),
+	router = require('koa-router')(),
+	bodyParser = require('koa-bodyparser'),
+	cors = require('koa2-cors')
+
+const app = new Koa()
+
+app.listen(3000)
+
+
+//路由相关设置
+app
+	.use(cors()) //允许跨域
+	.use(bodyParser()) //post请求参数
+	.use(router.routes()) //启动路由
+	.use(router.allowedMethods()) //自动设置请求头
+
+	//中间件
+	.use((ctx, next) => {
+		next()
+
+		//路径执行完成后执行下面代码
+		if (ctx.status == '404') {
+			console.log({
+				url:ctx.request.url,
+				type:ctx.request.method
+			})
+		}
+	})
+
+
+
+
+router.get('/',async ctx => {
+	ctx.body = "Hello World11"
+	
+})
---
+++ proxy.js
@@ -0,0 +1,22 @@
+const http = require('http'),
+	httpProxy = require('http-proxy');
+
+// 新建一个代理 Proxy Server 对象
+var proxy = httpProxy.createProxyServer({});
+
+http.createServer(function(req,res){
+// 在这里可以自定义路由分发
+	var host = req.headers.host
+	switch (host) {
+		case 'ccskill.cn':
+			proxy.web(req, res, {
+				target: 'http://127.0.0.1:3000'
+			});
+			break;
+		default:
+			res.writeHead(200, {
+				'Content-Type': 'text/plain'
+			});
+			res.end('Welcome to my server!');
+	}
+}).listen(80)
