a7eae01386b596b3fa9b1f8d96328e6b4b5ea5d6
Jy9 <33743739+Jy9@users.noreply.github.com>
2019-12-24 21:25:50 +0800

    asd

---
+++ udp.js
@@ -0,0 +1,19 @@
+const dgram = require('dgram');
+const server = dgram.createSocket('udp4');
+
+server.on('error', (err) => {
+  console.log(`服务器异常：\n${err.stack}`);
+  server.close();
+});
+
+server.on('message', (msg, rinfo) => {
+  console.log(`服务器接收到来自 ${rinfo.address}:${rinfo.port} 的 ${msg}`);
+  //server.send('lalala')
+});
+
+server.on('listening', () => {
+  const address = server.address();
+  console.log(`服务器监听 ${address.address}:${address.port}`);
+});
+
+server.bind(8888);
